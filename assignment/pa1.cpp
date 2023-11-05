7078aea1e768af994de76920bfed75658c08befe
tpgus2603 <kakaneymar2424@gmail.com>
2023-11-05 22:59:44 +0900

    add

--- assignment/pa1.cpp
+++ assignment/pa1.cpp
@@ -1,46 +1,46 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-int calc(const string &str,int s,int e)
+//시간복잡도 개선을 위해 범위내에 +가없는게 확실하면 +찾기 넘어감
+int calc(const string &str, int s, int e, bool isplus=true)  
 {
   int result;
-  bool isplus=false;
-  if(e-s==1)
-    return str[s]-'0';
-  else if(e-s>1)
+  if (e - s == 1)
+    return str[s] - '0';
+  else if (e - s > 1)
+  {
+    if (isplus) 
     {
-      for(int i=s;i<e;i++) //+찾기 
+      for (int i = s; i < e; i++) //+찾기
       {
-        if(str[i]=='+')
+        if (str[i] == '+')
         {
-          result=calc(str,s,i)+calc(str,i+1,e);
-          isplus=true;
+          result = calc(str, s, i,false) + calc(str, i + 1, e); //+앞부분은 +가없으니 false전달 
+          break;
+        }
+        if(i==e-1) //끝까지 +못찾은경우 
+          isplus=false;
+      }
+    }
+    if (!isplus) //+가 없으면 *찾기 해당 함수 범위내엔 +가없는게 확실하니 false
+    {
+      for (int i = s; i < e; i++)
+      {
+        if (str[i] == '*')
+        {
+          result = calc(str, s, i,false) * calc(str, i + 1,e, false); 
           break;
         }
       }
-      if(!isplus) //+가 없으면 -찾기 
-      {
-        for(int i=s;i<e;i++) 
-        {
-          if(str[i]=='*')
-          {
-            result=calc(str,s,i)*calc(str,i+1,e);
-            break;
-          }
-        }
-      }
     }
+  }
   return result;
-  
 }
 int main()
 {
   string str;
-  cin>>str;
-  cout<<calc(str,0,str.size());
-
-
-
+  cin >> str;
+  cout << calc(str, 0, str.size());
 }
---
+++ assignment/pa2.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+bool dp[1001][1001];
+int arr[1001];
+int pre[1001];
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n,target;
+  cin>>n;
+  arr[0]=0;
+  int temp;
+  for(int i=1;i<=n;i++)
+  {
+    cin>>temp;
+    arr[i]=temp;
+  }
+  cin>>target;
+  // dp[i][j]= 배열의 i번째 까지원소를 고려해서 j를  완성이 가능한지 아닌지
+  dp[0][0]=true;
+  for(int i=1;i<=target;i++)
+    dp[0][i]=false;
+  for(int i=1;i<=n;i++)
+  {
+    for(int j=0;j<=target;j++)
+    {
+      if(dp[i-1][j]==true)
+      {  
+        dp[i][j]=true;
+      }
+      if(dp[i-1][j-arr[i]]==true||dp[i][j-arr[i]]==true)
+      {
+        dp[i][j]=true;
+        pre[j]=arr[i];
+      }
+    }
+  }
+  if(dp[n][target]==false)
+    cout<<"Impossible";
+  else
+  {
+    for(int cur=target;cur!=0;cur-=pre[cur])
+      cout<<pre[cur]<<' ';
+  }
+
+}
